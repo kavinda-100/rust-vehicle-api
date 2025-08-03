f1e285785db273a791ef993afe096ab8c30a42ba
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-03 18:27:31 +0530

    add database configuration and update migration for user table

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -6,36 +6,34 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(User::Table)
                     .if_not_exists()
-                    .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(pk_auto(User::Id).uuid().primary_key())
+                    .col(string(User::Name).not_null())
+                    .col(string(User::Email).unique_key().not_null())
+                    .col(timestamp(User::CreatedAt).date_time().not_null())
+                    .col(timestamp(User::UpdatedAt).date_time().not_null())
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(User::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+enum User {
     Table,
     Id,
-    Title,
-    Text,
+    Name,
+    Email,
+    CreatedAt,
+    UpdatedAt,
 }
