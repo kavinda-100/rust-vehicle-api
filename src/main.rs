96f0f7b1ac6812a7ddd05f5e8179037e66accd00
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-03 20:09:29 +0530

    add endpoint to delete a user by UUID

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use sea_orm::ColumnTrait;
 use sea_orm::QueryFilter;
 mod models;
 
+use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
-use axum::routing::{get, patch, post};
+use axum::routing::{delete, get, patch, post};
 use axum::{Json, Router};
 use chrono::Utc;
+use chrono::format::Pad;
 use migration::Condition;
 use sea_orm::{ActiveModelTrait, Database, DatabaseConnection, EntityTrait, Set};
 use serde_json::json;
@@ -27,6 +29,7 @@ async fn main() {
         .route("/users", get(get_all_users))
         .route("/users-create", post(create_user))
         .route("/users-update", patch(update_user))
+        .route("/users-delete/{uuid}", delete(delete_user))
         // handle 404 not found
         .fallback(not_found);
 
@@ -145,6 +148,33 @@ async fn update_user(Json(u): Json<UserModelUpdate>) -> impl IntoResponse {
     (
         StatusCode::OK,
         Json(json!({"message": "User updated successfully", "user": updated_user})),
+    )
+}
+
+async fn delete_user(Path(uuid): Path<Uuid>) -> impl IntoResponse {
+    // Get database URL from environment variable or use a default
+    let db_connection = std::env::var("DATABASE_URL").unwrap_or_else(|_| {
+        "postgres://postgres:postgres@localhost:5432/rust-vehicle-api".to_string()
+    });
+    let db: DatabaseConnection = Database::connect(db_connection).await.unwrap();
+
+    let user_exists = entity::user::Entity::find()
+        .filter(Condition::all().add(user::Column::Id.eq(uuid)))
+        .one(&db)
+        .await
+        .unwrap();
+
+    if user_exists.is_none() {
+        return (
+            StatusCode::NOT_FOUND,
+            Json(json!({"error": "User does not exist"})),
+        );
+    }
+    user::Entity::delete_by_id(uuid).exec(&db).await.unwrap();
+    // This function is not implemented yet
+    (
+        StatusCode::OK,
+        Json(json!({"message": "User deleted successfully"})),
     )
 }
 
