98a02d37ea879317ae4f4bfd774c096acd0aa2e3
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-03 20:01:47 +0530

    add endpoints to fetch all users and update user details

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod models;
 
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
-use axum::routing::{get, post};
+use axum::routing::{get, patch, post};
 use axum::{Json, Router};
 use chrono::Utc;
 use migration::Condition;
@@ -12,7 +12,7 @@ use sea_orm::{ActiveModelTrait, Database, DatabaseConnection, EntityTrait, Set};
 use serde_json::json;
 use uuid::Uuid;
 
-use crate::models::user_model::{UserModel, UserModelCreate};
+use crate::models::user_model::{UserModel, UserModelCreate, UserModelUpdate};
 use entity::user;
 
 #[tokio::main]
@@ -24,7 +24,9 @@ async fn main() {
     let app = Router::new()
         .route("/", get(home))
         .route("/test", get(test))
+        .route("/users", get(get_all_users))
         .route("/users-create", post(create_user))
+        .route("/users-update", patch(update_user))
         // handle 404 not found
         .fallback(not_found);
 
@@ -35,6 +37,28 @@ async fn main() {
         Ok(_) => println!("Server running on http://localhost:5000"),
         Err(e) => eprintln!("Server error: {}", e),
     }
+}
+
+async fn get_all_users() -> impl IntoResponse {
+    // Get database URL from environment variable or use a default
+    let db_connection = std::env::var("DATABASE_URL").unwrap_or_else(|_| {
+        "postgres://postgres:postgres@localhost:5432/rust-vehicle-api".to_string()
+    });
+    let db: DatabaseConnection = Database::connect(db_connection).await.unwrap();
+
+    let users = entity::user::Entity::find().all(&db).await.unwrap();
+    let users: Vec<UserModel> = users
+        .into_iter()
+        .map(|user| UserModel {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+        })
+        .collect();
+
+    Json(json!({"users": users}))
 }
 
 async fn create_user(Json(u): Json<UserModelCreate>) -> impl IntoResponse {
@@ -69,15 +93,58 @@ async fn create_user(Json(u): Json<UserModelCreate>) -> impl IntoResponse {
     };
 
     let created_user = new_user.insert(&db).await.unwrap();
+    let created_user = UserModel {
+        id: created_user.id,
+        name: created_user.name,
+        email: created_user.email,
+        created_at: created_user.created_at,
+        updated_at: created_user.updated_at,
+    };
     (
         StatusCode::CREATED,
-        Json(json!({
-            "id": created_user.id,
-            "name": created_user.name,
-            "email": created_user.email,
-            "created_at": created_user.created_at,
-            "updated_at": created_user.updated_at,
-        })),
+        Json(json!({"message": "User created successfully", "user": created_user})),
+    )
+}
+
+async fn update_user(Json(u): Json<UserModelUpdate>) -> impl IntoResponse {
+    // Get database URL from environment variable or use a default
+    let db_connection = std::env::var("DATABASE_URL").unwrap_or_else(|_| {
+        "postgres://postgres:postgres@localhost:5432/rust-vehicle-api".to_string()
+    });
+    let db: DatabaseConnection = Database::connect(db_connection).await.unwrap();
+
+    let user_exists = entity::user::Entity::find()
+        .filter(Condition::all().add(user::Column::Id.eq(u.id.clone())))
+        .one(&db)
+        .await
+        .unwrap();
+
+    if user_exists.is_none() {
+        return (
+            StatusCode::NOT_FOUND,
+            Json(json!({"error": "User does not exist"})),
+        );
+    }
+
+    let mut user: user::ActiveModel = user_exists.unwrap().into();
+    if let Some(name) = u.name {
+        user.name = Set(name);
+    }
+    if let Some(email) = u.email {
+        user.email = Set(email);
+    }
+
+    let updated_user = user.update(&db).await.unwrap();
+    let updated_user = UserModel {
+        id: updated_user.id,
+        name: updated_user.name,
+        email: updated_user.email,
+        created_at: updated_user.created_at,
+        updated_at: updated_user.updated_at,
+    };
+    (
+        StatusCode::OK,
+        Json(json!({"message": "User updated successfully", "user": updated_user})),
     )
 }
 
